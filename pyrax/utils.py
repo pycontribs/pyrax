8b67df7fd1a994f55b3309a60ead4738d8336dbf
EdLeafe <ed@leafe.com>
2012-10-07 09:03:21 -0500

    Added SelfDeletingTempDirectory class and folder_size() method to utils.py

--- pyrax/utils.py
+++ pyrax/utils.py
@@ -1,9 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import fnmatch
 import hashlib
 import os
+import shutil
 import tempfile
+
+import pyrax.exceptions as exc
+
+
+class SelfDeletingTempfile(object):
+    name = None
+
+    def __enter__(self):
+        fd, self.name = tempfile.mkstemp()
+        os.close(fd)
+        return self.name
+
+    def __exit__(self, type, value, traceback):
+        os.unlink(self.name)
+
+
+class SelfDeletingTempDirectory(object):
+    name = None
+
+    def __enter__(self):
+        self.name = tempfile.mkdtemp()
+        return self.name
+
+    def __exit__(self, type, value, traceback):
+        shutil.rmtree(self.name)
 
 
 def get_checksum(content):
@@ -24,13 +51,27 @@ def get_checksum(content):
     return md.hexdigest()
 
 
-class SelfDeletingTempfile(object):
-    name = None
+def folder_size(pth, ignore=None):
+    if not os.path.isdir(pth):
+        raise exc.FolderNotFound
+    if ignore:
+        if not isinstance(ignore, (list, tuple)):
+            ignore = [ignore]
+    else:
+        ignore = []
 
-    def __enter__(self):
-        fd, self.name = tempfile.mkstemp()
-        os.close(fd)
-        return self.name
+    def get_size(total, root, names):
+        paths = [os.path.realpath(os.path.join(root, nm)) for nm in names]
+        for pth in paths[::-1]:
+            if not os.path.exists(pth):
+                paths.remove(pth)
+            for pattern in ignore:
+                if fnmatch.fnmatch(pth, pattern):
+                    paths.remove(pth)
+                    break
+        total[0] += sum(os.stat(pth).st_size for pth in paths)
 
-    def __exit__(self, type, value, traceback):
-        os.unlink(self.name)
+    # Need a mutable to pass
+    total = [0]
+    os.path.walk(pth, get_size, total)
+    return total[0]
