4aed54f97f19b3cbb6cb28a2a1485889dd0e870c
Ed Leafe <ed@leafe.com>
2012-11-26 21:46:47 +0000

    Added additional class doc files.

--- docs/html/search/variables_64.js
+++ docs/html/search/variables_64.js
@@ -7,7 +7,5 @@ var searchData=
   ['description',['description',['../namespacesetup.html#a23af17c78302b71c14ef38ea40b8d1d7',1,'setup']]],
   ['details',['details',['../classpyrax_1_1exceptions_1_1ClientException.html#ace24e4da2dacc04a319964f1507ddb87',1,'pyrax::exceptions::ClientException']]],
   ['display_5fdescription',['display_description',['../classpyrax_1_1cloudblockstorage_1_1CloudBlockStorageSnapshot.html#a0659160d55e1ea0dfc7009027dfcb232',1,'pyrax::cloudblockstorage::CloudBlockStorageSnapshot::display_description()'],['../classpyrax_1_1cloudblockstorage_1_1CloudBlockStorageVolume.html#a0659160d55e1ea0dfc7009027dfcb232',1,'pyrax::cloudblockstorage::CloudBlockStorageVolume::display_description()']]],
-  ['display_5fname',['display_name',['../classpyrax_1_1cloudblockstorage_1_1CloudBlockStorageSnapshot.html#a21c9c48354b5113b02f4863870624c34',1,'pyrax::cloudblockstorage::CloudBlockStorageSnapshot::display_name()'],['../classpyrax_1_1cloudblockstorage_1_1CloudBlockStorageVolume.html#a21c9c48354b5113b02f4863870624c34',1,'pyrax::cloudblockstorage::CloudBlockStorageVolume::display_name()']]],
-  ['docdir',['docdir',['../namespacereset__docs.html#a1aa933afd04a33680106ccb3320e48dd',1,'reset_docs']]],
-  ['docs',['docs',['../namespacereset__docs.html#afaa7db951e945183730f55b7eb6fb797',1,'reset_docs']]]
+  ['display_5fname',['display_name',['../classpyrax_1_1cloudblockstorage_1_1CloudBlockStorageSnapshot.html#a21c9c48354b5113b02f4863870624c34',1,'pyrax::cloudblockstorage::CloudBlockStorageSnapshot::display_name()'],['../classpyrax_1_1cloudblockstorage_1_1CloudBlockStorageVolume.html#a21c9c48354b5113b02f4863870624c34',1,'pyrax::cloudblockstorage::CloudBlockStorageVolume::display_name()']]]
 ];
--- docs/html/search/variables_6f.js
+++ docs/html/search/variables_6f.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['object_5fcount',['object_count',['../classpyrax_1_1cf__wrapper_1_1container_1_1Container.html#adf84fc89f9e8786591953166a1fd9d52',1,'pyrax::cf_wrapper::container::Container']]],
-  ['object_5fmeta_5fprefix',['object_meta_prefix',['../classpyrax_1_1cf__wrapper_1_1client_1_1CFClient.html#a5adbaa075a44196e9f26841c572f0639',1,'pyrax::cf_wrapper::client::CFClient']]],
-  ['out',['out',['../namespacereset__docs.html#a8f279c25df319b3b1fd99ba63c5795a6',1,'reset_docs']]]
+  ['object_5fmeta_5fprefix',['object_meta_prefix',['../classpyrax_1_1cf__wrapper_1_1client_1_1CFClient.html#a5adbaa075a44196e9f26841c572f0639',1,'pyrax::cf_wrapper::client::CFClient']]]
 ];
