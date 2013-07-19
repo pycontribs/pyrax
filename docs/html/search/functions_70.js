da1984a9d04c8eee66e3503eaf4d801d8ab0a7fd
EdLeafe <ed@leafe.com>
2013-07-19 08:52:39 -0500

    Updated the class docs

--- docs/html/search/functions_70.js
+++ docs/html/search/functions_70.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['plug_5fhole_5fin_5fswiftclient_5fauth',['plug_hole_in_swiftclient_auth',['../namespacepyrax.html#a52520cf6c40b52d2b67faf9762accb18',1,'pyrax']]],
   ['projectid',['projectid',['../classpyrax_1_1client_1_1BaseClient.html#af6e68e7b4a48c30549646fd3d5ed1aae',1,'pyrax::client::BaseClient']]],
   ['protocols',['protocols',['../classpyrax_1_1cloudloadbalancers_1_1CloudLoadBalancerClient.html#a7ffc66e16cbb4a549e7874dce9d61e6e',1,'pyrax::cloudloadbalancers::CloudLoadBalancerClient']]],
   ['purge',['purge',['../classpyrax_1_1cf__wrapper_1_1storage__object_1_1StorageObject.html#a24447386d5cd26a6a61fd75fd19842c7',1,'pyrax::cf_wrapper::storage_object::StorageObject']]],
