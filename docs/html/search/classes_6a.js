553e562105f988d165870eb5d677306ad298fa24
EdLeafe <drg@leafe.com>
2014-03-12 16:03:04 -0500

    Updated the class docs

---
+++ docs/html/search/classes_6a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['jsonschemamanager',['JSONSchemaManager',['../classpyrax_1_1image_1_1JSONSchemaManager.html',1,'pyrax::image']]]
+];
---
+++ docs/html/search/files_69.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['image_2epy',['image.py',['../image_8py.html',1,'']]]
+];
